161d6484984f15f4087114da6d24b5c81ef6069d
Nick <nickrray@gmail.com>
2020-07-07 10:01:39 -0500

    :fire: Remove unnecessary preventDefault

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,10 @@
 // TODO: Use `querySelectorAll` to create a 'NodeList' of all of the 'buttons'
 // Iterate over that and add event listeners to each one.
 // The event listener should listen for a 'click.' When we click it should say 'clicked'
+const buttons = document.querySelectorAll('button')
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', (e) => {
+    alert('Clicked!')
+  })
+}
