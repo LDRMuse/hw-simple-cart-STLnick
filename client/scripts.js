804655882ef030fc29a78fa76bec5f5d35337c7a
Nick <nickrray@gmail.com>
2020-07-07 11:24:11 -0500

    :recycle: Change event listener loop from for to forEach

--- client/scripts.js
+++ client/scripts.js
@@ -3,8 +3,14 @@
 // The event listener should listen for a 'click.' When we click it should say 'clicked'
 const buttons = document.querySelectorAll('button')
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', (e) => {
+// for (let i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener('click', (e) => {
+//     alert('Clicked!')
+//   })
+// }
+
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => {
     alert('Clicked!')
   })
-}
+})
