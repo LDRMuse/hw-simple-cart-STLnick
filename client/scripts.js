2e4542550e9b6217b34149aa28e164ce0c0885ed
Nick <nickrray@gmail.com>
2020-07-08 15:38:45 -0500

    :fire: Clean up code and comments

--- client/scripts.js
+++ client/scripts.js
@@ -1,34 +1,27 @@
 const buttons = document.querySelectorAll('button')
 const cart = []
 
+// Check to see if an item is in the cart
 const checkInCart = (name, cart) => {
   let isInCart = false
-
   cart.forEach(item => {
     if (item.name === name) { isInCart = true }
   })
-
-  console.log(isInCart)
-
   return isInCart
 }
 
 buttons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    console.log('Clicked!')
-
     /* Add item to cart ========= */
     // Get item name
     const itemText = btn.previousElementSibling.querySelector('figcaption').textContent
-
     // If item is in cart increase qty by 1
     if (checkInCart(itemText, cart)) {
       const itemIndex = cart.findIndex(item => item.name === itemText)
       cart[itemIndex].qty++
       // Else add new object with itemText as name and qty 1
     } else {
-      const newItem = { name: itemText, qty: 1 }
-      cart.push(newItem)
+      cart.push({ name: itemText, qty: 1 })
     }
   })
 })
