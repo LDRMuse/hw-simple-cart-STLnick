92f240e705a8e2cf808dc8bd71fe4854ecf8806e
Nick <nickrray@gmail.com>
2020-07-08 15:16:54 -0500

    :sparkles: Implement function to check and add items to cart

--- client/scripts.js
+++ client/scripts.js
@@ -1,3 +1,31 @@
-// TODO: Use `querySelectorAll` to create a 'NodeList' of all of the 'buttons'
-// Iterate over that and add event listeners to each one.
-// The event listener should listen for a 'click.' When we click it should say 'clicked'
+const buttons = document.querySelectorAll('button')
+const cart = []
+
+const checkInCart = (name, cart) => {
+  let isInCart = false
+
+  cart.forEach((item) => {
+    if (item.name === name) { isInCart = true }
+  })
+
+  return isInCart
+}
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    console.log('CLICKED')
+    /* Add object to cart ======= */
+    // Grab text from clicked item
+    const itemText = e.target.previousElementSibling.querySelector('figcaption').textContent
+
+    // If item is in cart just increase qty by 1
+    if (checkInCart(itemText, cart)) {
+      const itemIndex = cart.findIndex(item => item.name === itemText)
+      cart[itemIndex].qty++
+    } else {
+      // Else add item and qty=1 to cart
+      const newItem = { name: itemText, qty: 1 }
+      cart.push(newItem)
+    }
+  })
+})
