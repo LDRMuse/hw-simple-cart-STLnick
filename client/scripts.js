ab9854e0842588e2037b27ad3b6713e22cb437ee
Nick <nickrray@gmail.com>
2020-07-09 14:09:25 -0500

    :recycle: Remove auxiliary check fxn for simple index check for adding item to cart

--- client/scripts.js
+++ client/scripts.js
@@ -1,23 +1,16 @@
 const buttons = document.querySelectorAll('button')
 const cart = []
 
-const checkInCart = (name, cart) => {
-  let isInCart = false
-  cart.forEach((item) => {
-    if (item.name === name) { isInCart = true }
-  })
-  return isInCart
-}
-
 buttons.forEach((btn) => {
   btn.addEventListener('click', (e) => {
-    console.log('CLICKED')
     /* Add object to cart ======= */
     // Grab text from clicked item
     const itemText = e.target.previousElementSibling.querySelector('figcaption').textContent
-    // If item is in cart just increase qty by 1
-    if (checkInCart(itemText, cart)) {
-      const itemIndex = cart.findIndex(item => item.name === itemText)
+
+    const itemIndex = cart.findIndex(item => item.name === itemText)
+
+    // If item is in cart (index is valid) just increase qty by 1
+    if (itemIndex !== -1) {
       cart[itemIndex].qty++
     } else {
       // Else add item and qty=1 to cart
