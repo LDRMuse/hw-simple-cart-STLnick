24c0535759ff9a8785acd1a8283f2e3afa28f40c
Nick Ray <nickrray@gmail.com>
2020-07-14 15:30:45 -0500

    :recycle: Refactor to dynamically generate html items using index.js - scripts functionality moved to root level index.js

---
+++ client/components/Item/index.js
@@ -0,0 +1,15 @@
+function render(divs) {
+  return divs.reduce((acc, cur) => {
+    return acc + `
+    <div>
+      <figure>
+        <img src="${cur.imgUrl}" alt="Random Image">
+        <figcaption>${cur.caption}</figcaption>
+      </figure>
+      <button type="button">Add 2 Cart <span>🛒</span></button>
+    </div>
+    `
+  }, '')
+}
+
+export default (props) => render(props)
---
+++ client/index.js
@@ -0,0 +1,77 @@
+import { Item } from './components'
+
+const HTMLRoot = document.querySelector('#root')
+
+const props = {
+  Item: [
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #1'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #2'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #3'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #4'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #5'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #6'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #7'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #8'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #9'
+    },
+    {
+      imgUrl: 'https://source.unsplash.com/random/320x240',
+      caption: 'Item #10'
+    }
+  ]
+}
+
+function render() {
+  HTMLRoot.innerHTML = Item(props.Item)
+
+  const buttons = document.querySelectorAll('button')
+  const cart = []
+
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      /* Add object to cart ======= */
+      // Grab text from clicked item
+      const itemText = e.target.previousElementSibling.querySelector('figcaption').textContent
+
+      const itemIndex = cart.findIndex(item => item.name === itemText)
+
+      // If item is in cart (index is valid) just increase qty by 1
+      if (itemIndex !== -1) {
+        cart[itemIndex].qty++
+      } else {
+        // Else add item and qty=1 to cart
+        const newItem = { name: itemText, qty: 1 }
+        cart.push(newItem)
+      }
+      console.log(cart)
+    })
+  })
+}
+
+render()
