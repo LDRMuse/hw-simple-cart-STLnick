5724d5d12ee366b4c958bd2fd7c072e65275b623
Nick <nickrray@gmail.com>
2020-07-13 20:10:04 -0500

    :recycle: Place scripts.js functionality inside index.js and delete scripts.js

--- client/index.js
+++ client/index.js
@@ -4,6 +4,27 @@ function render() {
   document.querySelector('#root').innerHTML = `
     ${Item()}
   `
+
+  const buttons = document.querySelectorAll('button')
+  const cart = []
+
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      /* Add object to cart ======= */
+      // Grab text from clicked item
+      const itemText = e.target.previousElementSibling.querySelector('figcaption').textContent
+      // Find index of item if it's in cart already OR get back -1
+      const itemIndex = cart.findIndex(item => item.name === itemText)
+
+      // If item is in cart (index is valid) just increase qty by 1
+      if (itemIndex !== -1) {
+        cart[itemIndex].qty++
+      } else { // Else add item and qty=1 to cart
+        const newItem = { name: itemText, qty: 1 }
+        cart.push(newItem)
+      }
+    })
+  })
 }
 
 render()
